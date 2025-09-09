a08003d204353635dc8ce1e9965c4ed28f91dafe
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-10 06:01:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const gameLinks = {
-  mahjong: "https://m.pgsoft-games.com/mahjong-ways-3/index.html",
-  luckyneko: "https://m.pgsoft-games.com/lucky-neko/index.html",
-  treasures: "https://m.pgsoft-games.com/treasures-aztec/index.html"
-};
+function openGame(url) {
+  window.open(url, "_blank");
+}
