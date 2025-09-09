3e956811c31b63744571e0ae1cbc72c40e0a715a
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-10 05:45:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 const gameLinks = {
-  mahjong: "https://d3pvfi6m7bxu71.cloudfront.net/gs2c/html5Game.do?extGame=1&symbol=vswaysmahwblck&gname=Mahjong%20Wins%203%20-%20Black%20Scatter&jurisdictionID=99&mgckey=stylename@generic~SESSION@demo",
-  zeus: "https://demogamesfree.pragmaticplay.net/gs2c/html5Game.do?extGame=1&symbol=vs20olympgate&jurisdictionID=99&mgckey=stylename@generic~SESSION@demo",
-  sweet: "https://demogamesfree.pragmaticplay.net/gs2c/html5Game.do?extGame=1&symbol=vs20sweetbonanza&jurisdictionID=99&mgckey=stylename@generic~SESSION@demo"
+  mahjong: "https://m.pgsoft-games.com/mahjong-ways-3/index.html",
+  luckyneko: "https://m.pgsoft-games.com/lucky-neko/index.html",
+  treasures: "https://m.pgsoft-games.com/treasures-aztec/index.html"
 };
-
-const gameFrame = document.getElementById("gameFrame");
-const gameCards = document.querySelectorAll(".game-card");
-
-// fungsi load game
-function loadGame(key) {
-  gameFrame.src = gameLinks[key];
-}
-
-// kasih event click ke semua card
-gameCards.forEach(card => {
-  card.addEventListener("click", () => {
-    const key = card.getAttribute("data-game");
-    loadGame(key);
-  });
-});
-
-// default pertama kali load Mahjong
-loadGame("mahjong");
